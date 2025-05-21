c29a5f2e62d34013ee4564ca484cbe2b911fdcc8
mahajialirezaei <m.a.hajialirezaei05@gmail.com>
2025-05-22 00:00:10 +0330

    complete it using example

--- word-search.py
+++ word-search.py
@@ -1,8 +1,20 @@
 import collections
+from os import remove
 
 found_words = []
 def check(i, j, m, n, visited):
     return i < m and j < n and i > 0 and j > 0 and [i, j] not in visited
+
+
+def search(board, words, m, n):
+    visited = set()
+    word = ''
+    for i in range(m):
+        for j in range(n):
+            visited.add([i, j])
+            word = board[i][j]
+            wordSearch(board, i, j, words, m, n, word, visited)
+
 
 
 def wordSearch(board, i, j, words, m, n, word, visited):
@@ -31,3 +43,18 @@ def wordSearch(board, i, j, words, m, n, word, visited):
         wordSearch(board, i, j - 1, words, m, n, word, visited)
         visited.remove([i, j - 1])
 
+
+
+board = [
+['o', 'a', 'a', 'n'],
+['e', 't', 'a', 'e'],
+['i', 'h', 'k', 'r'],
+['i', 'f', 'l', 'v']
+]
+
+words = ['oath', 'pea', 'eat', 'rain']
+
+
+m = 4
+n = 4
+search(board, words, m, n)
